35c6d1392a40e2e97f6ed2a6cd08473cb4c00530
Bruno-Lobo <brunofigueiredo_3@hotmail.com>
2020-07-06 16:46:20 -0300

    changes in src/app.js

---
+++ src/app.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+
+
+app.get("/repositories", (request, response) => {
+
+  return response.json(repositories)
+});
+
+
+app.post("/repositories", (request, response) => {
+  const { url, title, techs } = request.body
+  
+  const repository ={ 
+    id: uuid(),
+    url, 
+    title, 
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository)
+  return response.json(repository)
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params
+  const { url, title, techs } = request.body
+
+  const userId = repositories.findIndex(repository => repository.id === id)
+
+  if(userId<0){
+    return response.status(400).json([{
+      error: 'Bad Request'
+    }])
+  }
+  
+  const repository = { id, title, url, techs, likes: repositories[userId].likes }
+
+  repositories[userId] = repository
+
+  return response.status(200).json(repository)
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params
+
+  const userId = repositories.findIndex(repository => repository.id === id)
+  
+  if (userId < 0) {
+    return response.status(400).json([{
+      error: 'Bad Request'
+    }])
+  }
+
+  repositories.splice(userId, 1)
+
+  return response.status(204).send()
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params
+
+  const repository = repositories.find(repository => repository.id === id)
+
+  if(!repository){
+    
+    return response.status(400).json([{
+      error: 'Bad Request'
+    }])
+  }
+
+  repository.likes++
+
+  return response.json(repository)
+});
+
+module.exports = app;
